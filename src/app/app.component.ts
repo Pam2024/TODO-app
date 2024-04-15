200713f90693ada315b48ce5b24ea6b589cb20e8
Pamela Kuka <kukapamela2@gmail.com>
2024-04-15 15:11:13 +0200

    Commit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    TestComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo-app';
+  value: string = '';
+  allTodos: Array<Todo> = [];
+  valuesToShow: Array<Todo> = [];
+
+  addTodo() {
+    if (this.value.trim() == '') {
+      return;
+    }
+
+    var newValue:Todo={
+      title:this.value,
+      completed:false
+    };
+
+    this.allTodos.push(newValue);
+    this.valuesToShow.push(newValue);
+    this.value='';
+  }
+
+  filter(type: string){
+    if (type == 'inprogress') {
+      this.valuesToShow = this.allTodos.filter(x=>x.completed==false)
+    }else if(type == 'done'){
+      this.valuesToShow = this.allTodos.filter(x=>x.completed==true)
+    }else{
+      this.valuesToShow = this.allTodos
+    }
+  }
+}
+
+
+interface Todo {
+  title:string;
+  completed:boolean;
 }
---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+  title = 'todo-app';
+
+  greet(){
+    alert('HELLO WORLD!')
+  }
+
+}
